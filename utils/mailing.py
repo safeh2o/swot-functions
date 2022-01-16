00165662abe546dad9d18eccd120f58885572d81
Mohamed Moselhy <look4d@gmail.com>
2022-01-16 17:09:52 -0500

    add analyze url to upload summary

--- utils/mailing.py
+++ utils/mailing.py
@@ -4,9 +4,11 @@ from sendgrid.helpers.mail import Mail
 
 
 def send_mail(email, errors):
+    WEBURL = os.environ.get("WEBURL")
+    analyze_url = f"{WEBURL}/analyze"
     message = Mail(from_email="no-reply@safeh2o.app", to_emails=email)
     message.template_id = os.environ.get("SENDGRID_UPLOAD_SUMMARY_TEMPLATE_ID")
-    message.dynamic_template_data = {"errors": errors}
+    message.dynamic_template_data = {"errors": errors, "analyzeUrl": analyze_url}
     try:
         sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
         response = sg.send(message)
